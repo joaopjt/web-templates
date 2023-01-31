6e3ea59186d971a5091e23d73e829c0b7e1c90e6
Joao Julio <joaopjt@gmail.com>
2023-01-31 15:55:16 -0300

    multiple updates

--- resources/data/index.js
+++ resources/data/index.js
@@ -1,3 +1,7 @@
 module.exports = {
   user: 'John Doe'
+  banner: {
+    title: "First I wanted to be a veterinarian",
+    description: "Lorem Ipsum available but the majority have suffered alteration in some form, by injected humour randomised words."
+  },
 }
